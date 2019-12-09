98bad7b8d76d94e12fc71470563d10099a9b7aaa
lebach2000 <kenrou.hol0.the.wolf@gmail.com>
2019-12-09 17:30:37 +0700

    bai asigment

---
+++ ExamWAD/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ExamWAD.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ExamWAD.Models.ExamWADContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ExamWAD.Models.ExamWADContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ ExamWAD/Models/Exam.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExamWAD.Models
+{
+    public class Exam
+    {
+        public int Id { get; set; }
+        public string ExamSubject { get; set; }
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Time)]
+        public string StartTime { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public string ExamDate { get; set; }
+        public int ExamDuration { get; set; }
+        public string ClassRoom { get; set; }
+        public string ExamFaculty { get; set; }
+        public ExamStatus Status { get; set; }
+    }
+
+    public enum ExamStatus
+    {
+        Done = 0,
+        UpComing = 1,
+        OnGoing = -1
+    }
+}
